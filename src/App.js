ba161a38a7d85a39304625935376986c000e2fa2
aimee-liang <aimeeeliang@gmail.com>
2020-12-22 20:54:53 -0500

    Added projects component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = props => {
   return(
     <>
         <AboutMe/>
+        <Projects/>
     </>
   )
 }
---
+++ src/Components/Projects.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Projects = props => {
+    return (
+        <div className = "projects-container">
+            <h5>
+                Projects
+            </h5>
+            <div>
+                
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Projects
