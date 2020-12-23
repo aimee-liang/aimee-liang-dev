4782a2997d5597d81caffa66219df42272c99dcc
aimee-liang <aimeeeliang@gmail.com>
2020-12-22 20:33:30 -0500

    Updated README, created an aboutMe div and component

---
+++ src/Components/aboutMe.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const aboutMe = props => {
+    return(
+        <>
+            Hi! I'm Aimee, and I am a Fullstack Web Developer and Software Engineer. 
+        </>
+    )
+}
+export default aboutMe
