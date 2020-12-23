a110448d9894b74dac02d7d1f81d6ed88b221df8
aimee-liang <aimeeeliang@gmail.com>
2020-12-22 20:45:46 -0500

    Moved ProfilePic from App to AboutMe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react"
 import AboutMe from "./Components/AboutMe"
-import ProfilePic from "./Components/ProfilePic"
 
 const App = props => {
   return(
     <>
-        <ProfilePic/>
         <AboutMe/>
     </>
   )
