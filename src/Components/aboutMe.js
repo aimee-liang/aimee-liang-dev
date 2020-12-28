068a2da4a876d42498337f57121f7c80210ca41e
aimee-liang <aimeeeliang@gmail.com>
2020-12-28 18:19:52 -0500

    Imported Google fonts for typewriter effect

--- src/Components/aboutMe.js
+++ src/Components/aboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = props => {
             <ProfilePic/>
 
             <div className = "about-me container">
-
+                <p className="about-me-animation">Hi, I'm Aimee!</p>
                 <p>I'm a Fullstack Web Developer and Software Engineer. I love coding, traveling, and learning. 
                 I’m a New Yorker born and raised, but spent a significant amount of my life in Washington D.C. That’s what shaped my decision to go into Tech. 
                 I knew I wanted a career where I could pursue my interests and find something that would align with my values, and where I could build meaningful and creative solutions to help make people’s lives easier through innovative technologies. 
