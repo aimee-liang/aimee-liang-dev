e34313fd2c18e0b3e0955fd4536c78d73aa78889
aimee-liang <aimeeeliang@gmail.com>
2020-12-28 21:15:30 -0500

    Imported CSS but typewriter effect incorrect

--- src/Components/aboutMe.js
+++ src/Components/aboutMe.js
@@ -1,22 +1,23 @@
 import React from "react"
 import ProfilePic from "./ProfilePic"
+import "../App.css"
 
 const AboutMe = props => {
     return(
         <>
             <ProfilePic/>
 
-            <div className = "about-me container">
-                <p className="about-me animation-type">Hi, I'm Aimee!</p>
-                <p className="about-me-p">I'm a Fullstack Web Developer and Software Engineer. I love coding, traveling, and learning. 
+            <div className="about-me-container">
+                <p className="about-me anim-typewriter">Hi, I'm Aimee!</p>
+                <p className="me-paragraph">I'm a Fullstack Web Developer and Software Engineer.
                 <br></br>
                 I’m a New Yorker born and raised, but spent a significant amount of my life in Washington D.C. 
                 <br></br>
                 That’s what shaped my decision to go into Tech. 
                 <br></br>
-                I knew I wanted a career where I could pursue my interests and find something that would align with my values, 
-                <br></br>
-                and where I could build meaningful and creative solutions to help make people’s lives easier through innovative technologies. 
+                I knew I wanted a career where I could pursue my interests, and build meaningful solutions
+                <br></br> 
+                to help make people’s lives easier through innovative technologies. 
                 </p>
             </div>
         </>
