c23fc10f434d43717cb46aa103f0391e81d89caf
aimee-liang <aimeeeliang@gmail.com>
2020-12-29 18:13:08 -0500

    Updated links to frontend repos

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,13 +3,30 @@ import React from "react"
 const Projects = props => {
     return (
         <div className="projects-container">
+
             <h3>
                 Projects
             </h3>
+
             <div className="tetris">
                 <div className="tetris-image">
                     <img alt="tetris" src=""/>
                 </div>
+                <a href="https://github.com/aimee-liang/tetris">Repo</a>
+            </div>
+
+            <div className="ubike">
+                <div className="ubike-image">
+                    <img alt="ubike" src="" />
+                </div>
+                <a href="https://github.com/aimee-liang/ubike-frontend">Repo</a>
+            </div>
+
+            <div className="munch">
+                <div className="munch-image">
+                    <img alt="munch" src="" />
+                </div>
+                <a href="https://github.com/aimee-liang/munch-frontend">Repo</a>
             </div>
 
         </div>
