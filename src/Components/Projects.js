4784d549339f37bb889a1c843cffccdd8be3573d
aimee-liang <aimeeeliang@gmail.com>
2020-12-29 18:52:42 -0500

    Added links to Projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,30 +5,45 @@ const Projects = props => {
         <>
             <h3 className="projects-header">Projects</h3>
 
-        <div className="projects-container">
+            <div className="projects-container">
 
-            <div className="tetris">
-                <div className="tetris-image">
-                    <img alt="tetris" src=""/>
+                <div className="tetris">
+                    <div className="tetris-image">
+                        <img alt="tetris" src=""/>
+                    </div>
+                    <a href="https://github.com/aimee-liang/tetris"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
                 </div>
-                <a href="https://github.com/aimee-liang/tetris"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
-            </div>
 
-            <div className="ubike">
-                <div className="ubike-image">
-                    <img alt="ubike" src="" />
+                <div className="ubike">
+                    <div className="ubike-image">
+                        <img alt="ubike" src="" />
+                    </div>
+                    <a href="https://github.com/aimee-liang/ubike-frontend"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
                 </div>
-                <a href="https://github.com/aimee-liang/ubike-frontend"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
-            </div>
 
-            <div className="munch">
-                <div className="munch-image">
-                    <img alt="munch" src="" />
+                <div className="munch">
+                    <div className="munch-image">
+                        <img alt="munch" src="" />
+                    </div>
+                    <a href="https://github.com/aimee-liang/munch-frontend"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
                 </div>
-                <a href="https://github.com/aimee-liang/munch-frontend"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
-            </div>
 
-        </div>
+                <div className="flatiron-trivia">
+                    <div className="flatiron-trivia-image">
+                        <img alt="flatiron-trivia" src="" />
+                    </div>
+                    <a href="https://github.com/aimee-liang/flatiron-trivia-frontend"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
+                </div>
+
+                <div className="kl-finance">
+                    <div className="kl-finance-image">
+                        <img alt="kl-finance" src="" />
+                    </div>
+                    <a href="https://github.com/aimee-liang/Mod2-Final-Project-Financial-App"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
+                </div>
+
+
+            </div>
 
         </>
     )
