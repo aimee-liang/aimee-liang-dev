61b35c6c12b0a4f4d3b8dc9ca27a657729e85c35
aimee-liang <aimeeeliang@gmail.com>
2020-12-29 18:24:28 -0500

    Added GitHub icons to repos

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,21 +12,21 @@ const Projects = props => {
                 <div className="tetris-image">
                     <img alt="tetris" src=""/>
                 </div>
-                <a href="https://github.com/aimee-liang/tetris">Repo</a>
+                <a href="https://github.com/aimee-liang/tetris"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
             </div>
 
             <div className="ubike">
                 <div className="ubike-image">
                     <img alt="ubike" src="" />
                 </div>
-                <a href="https://github.com/aimee-liang/ubike-frontend">Repo</a>
+                <a href="https://github.com/aimee-liang/ubike-frontend"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
             </div>
 
             <div className="munch">
                 <div className="munch-image">
                     <img alt="munch" src="" />
                 </div>
-                <a href="https://github.com/aimee-liang/munch-frontend">Repo</a>
+                <a href="https://github.com/aimee-liang/munch-frontend"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/>Repo</a>
             </div>
 
         </div>
