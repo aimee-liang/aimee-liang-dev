632ea6e4a5a2e627cd1aa58aeb38af5e28201bdc
aimee-liang <aimeeeliang@gmail.com>
2020-12-30 18:56:33 -0500

    Added description and tech tags to projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,18 +12,29 @@ const Projects = props => {
                         <img alt="tetris" src=""/>
                     </div>
 
+                    <div className="description">
+                        <p>Classic Tetris app built using vanilla JS on the Frontend</p>
+                    </div>
+
                     <div className="tetris-links">
                         <a href="https://github.com/aimee-liang/tetris"><img alt=""src="https://img.icons8.com/carbon-copy/40/000000/github.png"/></a>
                     </div>
 
                     <ul className="project-tech-list">
-
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
                     </ul>
                 </div>
+
 
                 <div className="ubike">
                     <div className="ubike-image">
                         <img alt="ubike" src="" />
+                    </div>
+
+                    <div className="description">
+
                     </div>
 
                     <div className="ubike-links">
@@ -35,9 +46,14 @@ const Projects = props => {
                     </ul>
                 </div>
 
+
                 <div className="munch">
                     <div className="munch-image">
                         <img alt="munch" src="" />
+                    </div>
+
+                    <div className="description">
+                        <p></p>
                     </div>
 
                     <div className="munch-links">
@@ -45,13 +61,21 @@ const Projects = props => {
                     </div>
 
                     <ul className="project-tech-list">
-                        
+                        <li>React</li>
+                        <li>Rails</li>
+                        <li>React Bootstrap</li>
+                        <li>API</li>
                     </ul>
                 </div>
+
 
                 <div className="flatiron-trivia">
                     <div className="flatiron-trivia-image">
                         <img alt="flatiron-trivia" src="" />
+                    </div>
+
+                    <div className="description">
+                        <p>Trivia game built using vanilla JavaScript</p>
                     </div>
 
                     <div className="flatiron-trivia-links">
@@ -59,13 +83,21 @@ const Projects = props => {
                     </div>
 
                     <ul className="project-tech-list">
-                        
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                        <li>Rails</li>
+                        <li>CSS</li>
                     </ul>
                 </div>
+
 
                 <div className="kl-finance">
                     <div className="kl-finance-image">
                         <img alt="kl-finance" src="" />
+                    </div>
+
+                    <div className="description">
+                        <p>Static finance website built using Rails backend</p>
                     </div>
 
                     <div className="kl-finance-links">
@@ -73,7 +105,10 @@ const Projects = props => {
                     </div>
 
                     <ul className="project-tech-list">
-                        
+                        <li>Ruby</li>
+                        <li>CSS</li>
+                        <li>Rails</li>
+                        <li>Bootstrap</li>
                     </ul>
                 </div>
 
