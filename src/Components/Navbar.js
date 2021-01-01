cee8af292fda601abdc6fad77fc8bcd0a38aaa94
aimee-liang <aimeeeliang@gmail.com>
2021-01-01 17:46:38 -0500

    Made size of icons in Navbar smaller

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,10 +3,10 @@ import React from "react"
 const Navbar = (props) => {
     return (
         <div className="navigation-bar">
-            <a href="https://github.com/@aimee-liang"><img alt=""src="https://img.icons8.com/ios/80/000000/github.png"/></a>
-            <a href="https://medium.com/@aimee_liang"><img alt="" src="https://img.icons8.com/ios/80/000000/medium-monogram--v1.png"/></a>
-            <a href="https://www.linkedin.com/in/aimeeliang/"><img alt=""src="https://img.icons8.com/ios/80/000000/linkedin.png"/></a>
-            <a href="mailto:aimeeeliang@gmail.com"><img alt="" src="https://img.icons8.com/carbon-copy/80/000000/email.png"/></a>
+            <a href="https://github.com/@aimee-liang"><img alt=""src="https://img.icons8.com/ios/70/000000/github.png"/></a>
+            <a href="https://medium.com/@aimee_liang"><img alt="" src="https://img.icons8.com/ios/70/000000/medium-monogram--v1.png"/></a>
+            <a href="https://www.linkedin.com/in/aimeeliang/"><img alt=""src="https://img.icons8.com/ios/70/000000/linkedin.png"/></a>
+            <a href="mailto:aimeeeliang@gmail.com"><img alt="" src="https://img.icons8.com/carbon-copy/70/000000/email.png"/></a>
         </div>
     )
 }
