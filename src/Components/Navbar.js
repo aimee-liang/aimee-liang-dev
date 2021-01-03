b2f1e9ad5b4824ada0d4684f77b7c2b97fd8f89c
aimee-liang <aimeeeliang@gmail.com>
2021-01-02 20:31:38 -0500

    Added design credit statement to Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,12 +2,19 @@ import React from "react"
 
 const Navbar = (props) => {
     return (
-        <div className="navigation-bar">
-            <a href="https://github.com/@aimee-liang"><img alt=""src="https://img.icons8.com/ios/70/000000/github.png"/></a>
-            <a href="https://medium.com/@aimee_liang"><img alt="" src="https://img.icons8.com/ios/70/000000/medium-monogram--v1.png"/></a>
-            <a href="https://www.linkedin.com/in/aimeeliang/"><img alt=""src="https://img.icons8.com/ios/70/000000/linkedin.png"/></a>
-            <a href="mailto:aimeeeliang@gmail.com"><img alt="" src="https://img.icons8.com/carbon-copy/70/000000/email.png"/></a>
-        </div>
+        <>
+            <div className="navigation-bar">
+                <a href="https://github.com/@aimee-liang"><img alt=""src="https://img.icons8.com/ios/70/000000/github.png"/></a>
+                <a href="https://medium.com/@aimee_liang"><img alt="" src="https://img.icons8.com/ios/70/000000/medium-monogram--v1.png"/></a>
+                <a href="https://www.linkedin.com/in/aimeeliang/"><img alt=""src="https://img.icons8.com/ios/70/000000/linkedin.png"/></a>
+                <a href="mailto:aimeeeliang@gmail.com"><img alt="" src="https://img.icons8.com/carbon-copy/70/000000/email.png"/></a>
+            </div>
+
+            <div className="design-credit">
+                <p>Design by Aimee Liang</p>
+            </div>
+
+        </>
     )
 }
 
