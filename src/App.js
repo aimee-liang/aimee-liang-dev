e91be57fd923e3585273a2b5d8fdbaef44bf6519
aimee-liang <aimeeeliang@gmail.com>
2021-01-04 15:17:37 -0500

    Removed dark mode toggler in app.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import Navbar from "./Components/Navbar"
 // import Header from "./Components/Header"
 import AboutMe from "./Components/AboutMe"
 import Projects from "./Components/Projects"
-import {ThemeProvider} from "styled-components"
-import {theme} from "./Components/Theme"
 
 const App = () => {
 
   return(
-    // <ThemeProvider theme={theme} toggleTheme={themeToggler}>
-      <>
+
+    <>
       <div className="app">
-        {/* <button onClick={themeToggler}>Change Modes</button> */}
           {/* <Header /> */}
           <AboutMe/>
           <Projects/>
           <Navbar/>
       </div>
-      </>
-    // </ThemeProvider>
+    </>
   )
 }
 
