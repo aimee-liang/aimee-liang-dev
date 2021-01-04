e7b088a85e4d10fc3de6f8f517e4c969dbcd71a9
aimee-liang <aimeeeliang@gmail.com>
2021-01-04 14:56:16 -0500

    Deleted useDarkMode.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,14 @@ import Navbar from "./Components/Navbar"
 // import Header from "./Components/Header"
 import AboutMe from "./Components/AboutMe"
 import Projects from "./Components/Projects"
-// import {ThemeProvider} from "styled-components"
-// import {theme} from "./Components/Theme"
-// import {useDarkMode} from "./Components/useDarkMode"
+import {ThemeProvider} from "styled-components"
+import {theme} from "./Components/Theme"
 
 const App = () => {
-
-  // const [theme, mountedComponent] = useDarkMode()
-  // const themeMode = theme === "light" ? lightTheme : darkTheme
-
-  // const themeToggler = () => {
-  //   theme === "light" ? setTheme("dark") : setTheme("light")
-  // }
 
   return(
     // <ThemeProvider theme={theme} toggleTheme={themeToggler}>
       <>
-      {/* <GlobalStyles/> */}
       <div className="app">
         {/* <button onClick={themeToggler}>Change Modes</button> */}
           {/* <Header /> */}
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -53,7 +53,7 @@ const Projects = props => {
                         </div>
 
                         <div className="tetris-links">
-                            <a href="https://google.com"><img alt=""src="https://img.icons8.com/carbon-copy/80/000000/domain.png"/></a>
+                            {/* <a href="https://google.com"><img alt=""src="https://img.icons8.com/carbon-copy/80/000000/domain.png"/></a> */}
                             <a href="https://github.com/aimee-liang/tetris"><img alt=""src="https://img.icons8.com/carbon-copy/60/000000/github.png"/></a>
                         </div>
 
