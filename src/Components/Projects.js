b0ea3153d45584cda0f89fc379c26ac12348413c
aimee-liang <aimeeeliang@gmail.com>
2021-01-05 13:47:10 -0500

    Changed styling for links and tech stack

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -78,12 +78,10 @@ const Projects = () => {
                             <span>#React</span>
                             <span>#Rails</span>
                             <span>#PostgresQL</span>
-                            <br></br>
                             <span>#JWT</span>
                             <span>#BCrypt</span>
                             <span>#API</span>
                             <span>#CSS</span>
-                            <br></br>
                             <span>#Material-UI</span>
                         </div>
 
@@ -107,16 +105,16 @@ const Projects = () => {
                             <p>Fullstack App to search restaurants based on cuisine and neighborhood, create and delete reservations</p>
                         </div>
 
-                        <ul className="project-tech-list">
-                            <li>React</li>
-                            <li>Rails</li>
-                            <li>React Bootstrap</li>
-                            <li>CSS</li>
-                            <li>API</li>
-                            <li>PostgresQL</li>
-                            <li>JWT</li>
-                            <li>BCrypt</li>
-                        </ul>
+                        <div className="project-tech-list">
+                            <span>#React</span>
+                            <span>#Rails</span>
+                            <span>#Bootstrap</span>
+                            <span>#CSS</span>
+                            <span>#API</span>
+                            <span>#PostgresQL</span>
+                            <span>#JWT</span>
+                            <span>#BCrypt</span>
+                        </div>
 
                         <div className="munch-links">
                             {/* <img alt=""src="https://img.icons8.com/nolan/60/web.png"/> */}
@@ -137,13 +135,13 @@ const Projects = () => {
                             <p>Fullstack Trivia app built using vanilla JavaScript</p>
                         </div>
 
-                        <ul className="project-tech-list">
-                            <li>JavaScript</li>
-                            <li>Bootstrap</li>
-                            <li>Rails</li>
-                            <li>CSS</li>
-                            <li>PostgresQL</li>
-                        </ul>
+                        <div className="project-tech-list">
+                            <span>#JavaScript</span>
+                            <span>#Bootstrap</span>
+                            <span>#Rails</span>
+                            <span>#CSS</span>
+                            <span>#PostgresQL</span>
+                        </div>
 
                         <div className="flatiron-trivia-links">
                             {/* <img alt=""src="https://img.icons8.com/nolan/60/web.png"/>     */}
@@ -164,13 +162,13 @@ const Projects = () => {
                             <p>Static finance website built using Rails backend</p>
                         </div>
 
-                        <ul className="project-tech-list">
-                            <li>Ruby</li>
-                            <li>CSS</li>
-                            <li>Rails</li>
-                            <li>SQLite3</li>
-                            <li>Bootstrap</li>
-                        </ul>
+                        <div className="project-tech-list">
+                            <span>#Ruby</span>
+                            <span>#CSS</span>
+                            <span>#Rails</span>
+                            <span>#SQLite3</span>
+                            <span>#Bootstrap</span>
+                        </div>
 
                         <div className="kl-finance-links">
                             {/* <img alt=""src="https://img.icons8.com/nolan/60/web.png"/> */}
@@ -188,11 +186,11 @@ const Projects = () => {
                             <p>Text adventure game based on James Cameron's movie</p>
                         </div>
 
-                        <ul className="project-tech-list">
-                            <li>Ruby</li>
-                            <li>ActiveRecord</li>
-                            <li>SQLite3</li>
-                        </ul>
+                        <div className="project-tech-list">
+                            <span>#Ruby</span>
+                            <span>#ActiveRecord</span>
+                            <span>#SQLite3</span>
+                        </div>
 
                         <div className="avatar-links">
                             <a href="docs.google.com"><img alt=""src="https://img.icons8.com/nolan/60/youtube-squared.png"/></a>
