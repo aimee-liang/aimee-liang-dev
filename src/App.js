d3809cc87254f78ffaac7bd91e1286333928835d
aimee-liang <aimeeeliang@gmail.com>
2021-01-08 19:43:47 -0500

    Removed Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Navbar from "./Components/Navbar"
-// import Header from "./Components/Header"
 import AboutMe from "./Components/AboutMe"
 import Projects from "./Components/Projects"
 
@@ -10,7 +9,6 @@ const App = () => {
 
     <>
       <div className="app">
-          {/* <Header /> */}
           <AboutMe/>
           <Projects/>
           <Navbar/>
