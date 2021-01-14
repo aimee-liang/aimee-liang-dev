fab7127a8ea283f520200397208bbcdf2d0a6417
aimee-liang <aimeeeliang@gmail.com>
2021-01-14 16:23:49 -0500

    Updated index.html and Medium link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
             <div className="connected">Stay connected</div>
             <div className="navigation-bar">
                 <a href="https://github.com/@aimee-liang"><img alt="" src="https://img.icons8.com/nolan/80/github.png"/></a>
-                <a href="https://medium.com/@aimee_liang"><img alt="" src="https://img.icons8.com/nolan/80/medium-new.png"/></a>
+                <a href="https://aimee-liang.medium.com"><img alt="" src="https://img.icons8.com/nolan/80/medium-new.png"/></a>
                 <a href="https://www.linkedin.com/in/aimeeliang/"><img alt="" src="https://img.icons8.com/nolan/80/linkedin.png"/></a>
                 <a href="mailto:hello@aimeeliang.com"><img alt="" src="https://img.icons8.com/nolan/80/email.png"/></a>
             </div>
