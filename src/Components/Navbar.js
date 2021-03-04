a06f467ffeb4f73a5d4e472b3a9036235154fd0a
aimee-liang <aimeeeliang@gmail.com>
2021-03-03 20:43:27 -0500

    Added src

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,10 +5,10 @@ const Navbar = () => {
         <>
             <div className="connected">Stay connected</div>
             <div className="navigation-bar">
-                <a href="https://github.com/aimee-liang"><img alt="" src="https://img.icons8.com/nolan/80/github.png"/></a>
-                <a href="https://aimee-liang.medium.com"><img alt="" src="https://img.icons8.com/nolan/80/medium-new.png"/></a>
-                <a href="https://www.linkedin.com/in/aimeeliang/"><img alt="" src="https://img.icons8.com/nolan/80/linkedin.png"/></a>
-                <a href="mailto:hello@aimeeliang.com"><img alt="" src="https://img.icons8.com/nolan/80/email.png"/></a>
+                <a href="https://github.com/aimee-liang"><img alt="github" src="https://img.icons8.com/nolan/80/github.png"/></a>
+                <a href="https://aimee-liang.medium.com"><img alt="medium" src="https://img.icons8.com/nolan/80/medium-new.png"/></a>
+                <a href="https://www.linkedin.com/in/aimeeliang/"><img alt="linkedin" src="https://img.icons8.com/nolan/80/linkedin.png"/></a>
+                <a href="mailto:hello@aimeeliang.com"><img alt="email" src="https://img.icons8.com/nolan/80/email.png"/></a>
             </div>
 
             <div className="design-credit">
