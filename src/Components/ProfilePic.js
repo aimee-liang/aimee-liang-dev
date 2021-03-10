48e461215a765374aae56f1ee80fbbc0dfb33847
aimee-liang <aimeeeliang@gmail.com>
2021-03-09 22:53:58 -0500

    trying to see whether img src matters

--- src/Components/ProfilePic.js
+++ src/Components/ProfilePic.js
@@ -3,7 +3,7 @@ import React from "react"
 const ProfilePic = () => {
     return(
         <div className="profile-pic-container">
-            <img alt="aimee" src="/aimee_liang_profile.png" />
+            <img alt="aimee liang" src="/aimee_liang_profile.png" />
         </div>
     )
 }
