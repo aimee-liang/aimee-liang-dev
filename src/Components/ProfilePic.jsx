dcc9ed91ff33b317ed15b60fbb1db87f12dc37b9
aimee-liang <aimeeeliang@gmail.com>
2021-05-23 22:50:32 -0400

    Refactoring

---
+++ src/Components/ProfilePic.jsx
@@ -0,0 +1,7 @@
+export default function ProfilePic (){
+    return(
+        <div className="profile-pic-container">
+            <img alt="aimee liang" src="/aimee_liang_profile.png" />
+        </div>
+    )
+}
