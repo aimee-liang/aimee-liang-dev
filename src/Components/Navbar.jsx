9b1f464ad2d7b9c0b2b3c81d30f346cc7cbc2f27
aimee-liang <aimeeeliang@gmail.com>
2021-05-24 16:06:25 -0400

    Updating Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,27 @@
+import styled from 'styled-components'
+
+const Connected = styled.div`
+    text-align: center;
+    margin-top: 10%;
+    margin-bottom: 5%;
+    color: white;
+    text-shadow: 0px 0px 10px white;
+    font-size: 30px;
+`
+
+const NavigationBar = styled.div`
+
+`
+
+const DesignCredit = styled.div`
+    
+`
+
 export default function Navbar(){
     return (
         <>
-            <div className="connected">Stay connected</div>
+            <Connected>Stay connected</Connected>
+            {/* <div className="connected">Stay connected</div> */}
             <div className="navigation-bar">
                 <a href="https://github.com/aimee-liang"><img alt="github" src="https://img.icons8.com/nolan/80/github.png"/></a>
                 <a href="https://aimee-liang.medium.com"><img alt="medium" src="https://img.icons8.com/nolan/80/medium-new.png"/></a>
